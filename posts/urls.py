c35f1e1edc679e09ef711097c1d1439f86c69cb6
aviyan7 <aviyandahal47@gmail.com>
2022-02-12 12:00:47 +0545

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import category, createPost,posts
+urlpatterns = [
+    path('',posts, name="posts"),
+    path('category/', category, name="category"),
+    path('create-post/', createPost, name="createPost"),
+]
---
+++ posts/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from .models import Category,Posts
+from django.contrib import messages
+from .forms import PostForm
+# Create your views here.
+
+def category(request):
+    if request.method=='GET':
+        cat = Category.objects.all()#select *
+        context = {
+            'categories':cat,
+        }
+        return render(request,'category.html',context)
+    else:
+        t = request.POST['categorytitle']
+        try:
+            cat = Category(title=t)
+            cat.save()
+            messages.add_message(request,messages.SUCCESS,"saved")
+        except Exception as e:
+            messages.add_message(request,messages.ERROR,e)
+        return redirect('category')
+
+def createPost(request):
+    if request.method=='GET':
+        form = PostForm()
+        context = {
+            'form':form
+        }
+        return render(request,'createpost.html',context)
+    else:
+        form = PostForm(request.POST,request.FILES or None)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,"saved")
+            return redirect('createpost')
+
+
+def posts(request):
+    posts = Posts.objects.all()
+    context = {
+       'posts':posts
+   }
+    return render(request, 'posts.html',context)
